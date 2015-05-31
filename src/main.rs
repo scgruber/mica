b319a159bb3edb17fdc371d8ae7c7dcbe9c5316f
Sam Gruber <grubermensch@gmail.com>
2015-05-30 18:57:10 -0700

    Add handlebars templating

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,41 @@
 extern crate iron;
+extern crate handlebars_iron as hbs;
+extern crate rustc_serialize;
 
 use iron::prelude::*;
 use iron::status;
+use hbs::{Template, HandlebarsEngine};
+use rustc_serialize::json::{ToJson, Json};
+use std::collections::BTreeMap;
+
+struct BlogPost {
+    title: String,
+    body: String
+}
+
+impl ToJson for BlogPost {
+    fn to_json(&self) -> Json {
+        let mut m: BTreeMap<String, Json> = BTreeMap::new();
+        m.insert("title".to_string(), self.title.to_json());
+        m.insert("body".to_string(), self.body.to_json());
+        m.to_json()
+    }
+}
+
+fn hello_world(_: &mut Request) -> IronResult<Response> {
+    let mut resp = Response::new();
+
+    let template_data = BlogPost {
+                            title: "Hello World!".to_string(),
+                            body: "Lorem ipsum dolor sit amet.".to_string()
+                        };
+    resp.set_mut(Template::new("blog_post", template_data)).set_mut(status::Ok);
+    Ok(resp)
+}
 
 fn main() {
-    fn hello_world(_: &mut Request) -> IronResult<Response> {
-        Ok(Response::with((status::Ok, "Hello World!")))
-    }
-
-    Iron::new(hello_world).http("localhost:3000").unwrap();
-    println!("On 3000");
+    let mut chain = Chain::new(hello_world);
+    chain.link_after(HandlebarsEngine::new("./templates/", ".hbs"));
+    println!("Server running on http://locahost:3000/");
+    Iron::new(chain).http("localhost:3000").unwrap();
 }
